eaf1b29ac154b4c2eb38a8e22ca5f83f0f638eb4
idn <aidinz@gmail.com>
2015-01-22 18:56:11 +0330

    Speed movement

--- Assets/Scripts/MoodMeter.cs
+++ Assets/Scripts/MoodMeter.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoodMeter : MonoBehaviour {
+public class MoodMeter : MonoBehaviour
+{
+	public float MaxMood = 100;
+	public float CurrentMood = 40;
 
-	// Use this for initialization
-	void Start () {
-	
+	public float MoodIncreaseAmountPerSecond = 3;
+	public float MoodDecrementPerSecond = -1;
+
+	private float MoodDelta = -1;
+
+	public void IncreaseMood ()
+	{
+		MoodDelta = MoodIncreaseAmountPerSecond;
+//		CurrentMood = CurrentMood > MaxMood ? MaxMood : CurrentMood;
 	}
-	
+
+	public void DecreaseMood ()
+	{
+		MoodDelta = MoodDecrementPerSecond;
+//		CurrentMood = CurrentMood <= 0 ? 0 : CurrentMood;
+	}
+
+//	void OnTriggerStay2D (Collider2D other)
+//	{
+//
+//	}
+
+//	void OnTriggerExit2D (Collider2D other)
+//	{
+//
+//	}
+
+	void UpdateMood()
+	{
+		CurrentMood += MoodDelta * Time.deltaTime;
+	}
+
+	void Start ()
+	{
+		MoodDelta = MoodDecrementPerSecond;
+	}
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		UpdateMood();
+		Debug.Log(CurrentMood);
 	}
 }
--- Assets/Scripts/RotateAround.cs
+++ Assets/Scripts/RotateAround.cs
@@ -1,42 +1,55 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class RotateAround : MonoBehaviour
 {
-		public GameObject Target;
-	public float rotationSpeed = 80.0f;
-
-//		public float radius = 2.0f;
-//		public float radiusSpeed = 0.5f;
-		
+	public GameObject Target;
+	public float RotationSpeed;
 	public Vector3 axis = -Vector3.forward;
+
+	public float SpeedMultiplier;
+	public float SpeedDivisioner;
 
 	private Transform targetTransform;
 	private Vector3 desiredPosition;
 
-		void Start ()
-		{
-				targetTransform = Target.transform;
-//				transform.position = (transform.position - center.position).normalized * radius + center.position;
-//				radius = 2.0f;
+	void OnTriggerEnter2D (Collider2D other)
+	{
+//		Debug.Log("increasing");
 
-//				axis = 
+		if (other.tag == "Planet")
+		{
+			other.gameObject.GetComponent<MoodMeter>().IncreaseMood();
+
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.tag == "Planet")
+		{
+			other.gameObject.GetComponent<MoodMeter>().DecreaseMood();
+			
+		}
+	}
+
+	void Start ()
+	{
+		targetTransform = Target.transform;
+	}
+
+	void Update ()
+	{
+		float tempRotateSpeed = RotationSpeed;
+
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			tempRotateSpeed *= SpeedMultiplier;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			tempRotateSpeed *= SpeedDivisioner;
 		}
 
-		void Update ()
-		{
-				transform.RotateAround (targetTransform.position, axis, rotationSpeed * Time.deltaTime);
-//				desiredPosition = (transform.position - center.position).normalized * radius + center.position;
-//				desiredPosition = (transform.position - center.position).normalized + center.position;
-//				transform.position = Vector3.MoveTowards (transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
-
-//				RotateLeft ();
-		}
-
-		void RotateLeft ()
-		{
-				Quaternion theRotation = transform.localRotation;
-				theRotation.z *= 270;
-				transform.localRotation = theRotation;
-		}
+		transform.RotateAround (targetTransform.position, axis, tempRotateSpeed * Time.deltaTime);
+	}
 }
