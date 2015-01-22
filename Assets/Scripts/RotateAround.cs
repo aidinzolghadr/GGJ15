33b3107482c528696bf7718c73079405aae8cc23
idn <aidinz@gmail.com>
2015-01-22 16:41:57 +0330

    Rotate around works.

--- Assets/Scripts/RotateAround.cs
+++ Assets/Scripts/RotateAround.cs
@@ -4,25 +4,39 @@ using System.Collections;
 public class RotateAround : MonoBehaviour
 {
 		public GameObject Target;
+	public float rotationSpeed = 80.0f;
 
-		public Transform center;
-		public Vector3 axis = Vector3.up;
-		public Vector3 desiredPosition;
-		public float radius = 2.0f;
-		public float radiusSpeed = 0.5f;
-		public float rotationSpeed = 80.0f;
+//		public float radius = 2.0f;
+//		public float radiusSpeed = 0.5f;
+		
+	public Vector3 axis = -Vector3.forward;
+
+	private Transform targetTransform;
+	private Vector3 desiredPosition;
 
 		void Start ()
 		{
-				center = Target.transform;
-				transform.position = (transform.position - center.position).normalized * radius + center.position;
-				radius = 2.0f;
+				targetTransform = Target.transform;
+//				transform.position = (transform.position - center.position).normalized * radius + center.position;
+//				radius = 2.0f;
+
+//				axis = 
 		}
 
 		void Update ()
 		{
-				transform.RotateAround (center.position, axis, rotationSpeed * Time.deltaTime);
-				desiredPosition = (transform.position - center.position).normalized * radius + center.position;
-				transform.position = Vector3.MoveTowards (transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+				transform.RotateAround (targetTransform.position, axis, rotationSpeed * Time.deltaTime);
+//				desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+//				desiredPosition = (transform.position - center.position).normalized + center.position;
+//				transform.position = Vector3.MoveTowards (transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+
+//				RotateLeft ();
+		}
+
+		void RotateLeft ()
+		{
+				Quaternion theRotation = transform.localRotation;
+				theRotation.z *= 270;
+				transform.localRotation = theRotation;
 		}
 }
