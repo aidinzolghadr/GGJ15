98771aa8c004334bafc51d8e4097e949700920fe
idn <aidinz@gmail.com>
2015-01-23 14:57:23 +0330

    Working on wave based spawner.

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -13,16 +13,91 @@ public class SpawnManager : MonoBehaviour
 
 	// TODO: Tutorial wave
 
-	public Wave[] StartWave = new Wave[]
+
+//	public Wave[] StartWave = new Wave[]
+//	{
+//		/* 0 */	new Wave { Positions =	new int[] {1},	MoodMin =  50,	MoodMax = 90},
+//		/* 1 */	new Wave { Positions =	new int[] {2},	MoodMin =  50,	MoodMax = 90},
+//		/* 2 */	new Wave { Positions =	new int[] {3},	MoodMin =  50,	MoodMax = 90},
+//		/* 3 */	new Wave { Positions =	new int[] {4},	MoodMin =  50,	MoodMax = 90},
+//	};
+
+	public Wave[] EasyWaves = new Wave[]
 	{
 		/* 0 */	new Wave { Positions =	new int[] {1},	MoodMin =  50,	MoodMax = 90},
+		/* 1 */	new Wave { Positions =	new int[] {2},	MoodMin =  50,	MoodMax = 90},
+		/* 2 */	new Wave { Positions =	new int[] {3},	MoodMin =  50,	MoodMax = 90},
+		/* 3 */	new Wave { Positions =	new int[] {4},	MoodMin =  50,	MoodMax = 90},
 	};
 
-	//TODO: Normal wave
+	public Wave[] MediumWaves = new Wave[]
+	{
+		/* 0 */	new Wave { Positions =	new int[] {1},	MoodMin =  50,	MoodMax = 90},
+		/* 1 */	new Wave { Positions =	new int[] {2},	MoodMin =  50,	MoodMax = 90},
+		/* 2 */	new Wave { Positions =	new int[] {3},	MoodMin =  50,	MoodMax = 90},
+		/* 3 */	new Wave { Positions =	new int[] {4},	MoodMin =  50,	MoodMax = 90},
+	};
+
+	public Wave[] HardWaves = new Wave[]
+	{
+		/* 0 */	new Wave { Positions =	new int[] {1},	MoodMin =  50,	MoodMax = 90},
+		/* 1 */	new Wave { Positions =	new int[] {2},	MoodMin =  50,	MoodMax = 90},
+		/* 2 */	new Wave { Positions =	new int[] {3},	MoodMin =  50,	MoodMax = 90},
+		/* 3 */	new Wave { Positions =	new int[] {4},	MoodMin =  50,	MoodMax = 90},
+	};
 
 	public int WaveIndex = 0;
+	public Wave CurrentWave;
 
 	public MoodMeter[] Planets;
+
+	public int MediumWaveIndexStarterNumber = 1;
+	public int HardWaveIndexStarterNumber 	= 3;
+
+	public const int MinimumAcceptableMood = 60;
+
+	bool currentWaveIsClear ()
+	{
+//		MoodMeter[] planets = GameObject.FindObjectsOfType<MoodMeter>();
+
+		for (int i = 0; i < Planets.Length; i++)
+		{
+			if (Planets[i].CurrentMood < MinimumAcceptableMood)
+				return false;
+		}
+
+		return true;
+	}
+
+	void LoadNextWave()
+	{
+
+
+		++WaveIndex;
+
+		// Calculating chances
+		int MediumWaveChance = (WaveIndex - MediumWaveIndexStarterNumber) * 0.05;
+		int HardWaveChance = (WaveIndex - HardWaveIndexStarterNumber) * 0.05;
+
+//		int EasyWaveChance = 1 - (MediumWaveChance + HardWaveChance);
+
+		int die = Random.value;
+
+		if (die <= MediumWaveChance)
+		{
+			CurrentWave = MediumWaves[Random.Range(0, MediumWaves.Length)];
+		}
+		else if (die <= MediumWaveChance + HardWaveChance)
+		{
+			CurrentWave = HardWaveChance[Random.Range(0, HardWaves.Length)];
+		}
+		else
+		{
+			CurrentWave = EasyWaves[Random.Range(0, EasyWaves.Length)];
+		}
+
+
+	}
 
 	void Awake()
 	{
@@ -32,7 +107,10 @@ public class SpawnManager : MonoBehaviour
 
 	void Start ()
 	{
-	
+		if (currentWaveIsClear())
+		{
+			LoadNextWave();
+		}
 	}
 	
 	void Update ()
